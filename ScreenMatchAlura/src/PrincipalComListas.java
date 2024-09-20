04c992356434455178fe0ca24125d4cf38c9d5c8
Murilo Mucci Santos <murilomucci@gmail.com>
2024-09-20 14:45:35 -0300

    Criado teste de listas
    Arquivo para  listas de de titulos assistidos, englobando filmes e séries.

---
+++ ScreenMatchAlura/src/PrincipalComListas.java
@@ -0,0 +1,26 @@
+package ScreenMatchAlura.src;
+
+import ScreenMatchAlura.src.br.com.alura.screenmatch.modelos.Filme;
+import ScreenMatchAlura.src.br.com.alura.screenmatch.modelos.Serie;
+import ScreenMatchAlura.src.br.com.alura.screenmatch.modelos.Titulo;
+import ScreenMatchAlura.src.br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas{
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme("O Poderoso Chefão" , 1970);
+        Serie lost = new Serie("Lost" , 2000);
+        Filme outroFilme = new Filme("Avatar", 2023);
+
+        ArrayList<Titulo> listaTitulos = new ArrayList<>();
+        listaTitulos.add(meuFilme);
+        listaTitulos.add(outroFilme);
+        listaTitulos.add(lost);
+        for(Titulo item: listaTitulos){
+            System.out.println(item);
+        }
+    }
+}
+
