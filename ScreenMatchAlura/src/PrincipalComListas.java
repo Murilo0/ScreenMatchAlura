b137602082400c86e2788e125c3d471dc0f2ee90
Murilo Mucci Santos <murilomucci@gmail.com>
2024-09-21 12:04:21 -0300

    Mudança condicional para mostrar classificação

--- ScreenMatchAlura/src/PrincipalComListas.java
+++ ScreenMatchAlura/src/PrincipalComListas.java
@@ -10,19 +10,24 @@ import java.util.ArrayList;
 
 public class PrincipalComListas{
     public static void main(String[] args) {
-
         Filme meuFilme = new Filme("O Poderoso Chefão" , 1970, 180);
         Serie lost = new Serie("Lost" , 2000);
         Filme outroFilme = new Filme("Avatar", 2023, 200);
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        meuFilme.avalia(9);
+
 
         ArrayList<Titulo> listaTitulos = new ArrayList<>();
         listaTitulos.add(meuFilme);
         listaTitulos.add(outroFilme);
         listaTitulos.add(lost);
         for(Titulo item: listaTitulos){
-            System.out.println(item);
-            Filme filme = (Filme) item;
-            System.out.println("Classificação: " + filme.getClassificacao());
+            System.out.println(item.getNome());
+            if (item instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
         }
     }
 }
